ef742340da75f99e3b7e60d27b3dd08b9e5610d5
David Shanske <david@shanske.com>
2018-05-20 23:40:38 -0500

    Add archive condition to theme

--- archive.php
+++ archive.php
@@ -36,12 +36,16 @@ get_header(); ?>
 				the_post();
 
 				/*
-				 * Include the Post-Format-specific template for the content.
+				 * Include the Post-Format or Post Kind-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
+				if ( class_exists( 'Kind_Taxonomy' ) ) {
+					get_template_part( 'template-parts/content', get_post_kind() );
+				}
+				else {
+					get_template_part( 'template-parts/content', get_post_format() );
+				}
 				// End the loop.
 			endwhile;
 
