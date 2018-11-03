f7c5c551752ca8bc927461e08ad45aa4622ff546
David Shanske <david@shanske.com>
2018-11-03 15:19:25 -0500

    Check for filter before removing

--- inc/plugin-support.php
+++ inc/plugin-support.php
@@ -7,13 +7,13 @@ function twenty_sixteen_indieweb_plugin_support() {
 	/*
  	* Adds support for Syndication Links
  	*/
-	if ( class_exists( 'Syn_Meta' ) ) {
+	if ( class_exists( 'Syn_Meta' ) && has_filter( 'the_content', array( 'Syn_Config', 'the_content' ) ) ) {
 		remove_filter( 'the_content', array( 'Syn_Config', 'the_content' ), 30 );
 	}
 	/*
 	 * Adds support for Simple Location
 	 */
-	if ( class_exists( 'Loc_View' ) ) {
+	if ( class_exists( 'Loc_View' ) && has_filter( 'the_content', array( 'Loc_View', 'location_content' ) ) ) {
 		remove_filter( 'the_content', array( 'Loc_View', 'location_content' ), 12 );
 	}
 
