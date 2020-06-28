d2902d5be98e4cf667976f0ad11f8fb959e7296c
David Shanske <david@shanske.com>
2020-06-28 19:35:50 +0000

    Add resource hinting, see TwentySixteen v1.9

--- functions.php
+++ functions.php
@@ -480,3 +480,24 @@ function iw26_widget_tag_cloud_args( $args ) {
 	return $args;
 }
 add_filter( 'widget_tag_cloud_args', 'iw26_widget_tag_cloud_args' );
+
+/**
+ * Add preconnect for Google Fonts.
+ *
+ * @since Twenty Sixteen 1.6
+ *
+ * @param array  $urls           URLs to print for resource hints.
+ * @param string $relation_type  The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function iw26_resource_hints( $urls, $relation_type ) {
+	if ( wp_style_is( 'iw26-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	}
+
+	return $urls;
+}
+add_filter( 'wp_resource_hints', 'iw26_resource_hints', 10, 2 );
