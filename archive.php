c95306bc8a3abd8508e78061d73c0ea31ecb549f
David Shanske <david@shanske.com>
2020-12-19 03:13:28 +0000

    Add name and summary to regular archive.

--- archive.php
+++ archive.php
@@ -26,8 +26,8 @@ get_header(); ?>
 			<header class="page-header">
 			<?php
 					the_archive_thumbnail();
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+					the_archive_title( '<h1 class="page-title p-name">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description p-summary">', '</div>' );
 
 			?>
 			<?php if ( class_exists( 'Simple_Location_Plugin' ) ) {
